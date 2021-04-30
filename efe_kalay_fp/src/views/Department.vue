6bb28d5490fbef41c38a580dee8d8acfe6a3159b
Efe Kalay <efekalay@Efe-MacBook-Pro.local>
2021-04-30 19:14:16 +0300

    department item objects has been extracted from api

--- efe_kalay_fp/src/views/Department.vue
+++ efe_kalay_fp/src/views/Department.vue
@@ -1,32 +1,68 @@
 <template>
   <div class="department">
-    <h1>This is {{displayName}} department items page </h1>
+    <h1>This is department items page {{ this.$route.params.id }}</h1>
+    <ul>
+      <li v-for="object in objects" :key="object.objectID">
+        <h3>{{object.objectName}}</h3>
+      </li>
+
+    </ul>
   </div>
-
 </template>
-
+<!-- eslint-disable -->
 <script>
-/* eslint-disable */
+
 export default {
   name: 'Department',
-  props: {
-    departmentId: Number,
-    displayName: String,
-  },
-
   data() {
     return {
-      items: [],
+      departmentId: Number,
+      objectDetails: Object,
+      objectIds: [],
+      objects: [],
     };
   },
+
+  methods: {
+    getItemDetails(num) {
+      console.log(`get item details ${num}`);
+      const url2 = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${num}`;
+      fetch(url2)
+        .then((response) => response.json())
+        .then((response) => {
+          this.objects.push(response);
+        });
+    },
+  },
+
   created() {
-    console.log('Department ID is... ' + `${this.$route.params.id}`);
-    const url = 'https://collectionapi.metmuseum.org/public/collection/v1/departments';
+    this.departmentId = this.$route.params.id;
+    const url = `https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=${this.$route.params.id}`;
     fetch(url)
       .then((response) => response.json())
       .then((response) => {
-        this.items = response.data;
+        this.objectIds = response.objectIDs.slice(0, 20);
+        this.objects = [];
+        response.objectIDs.slice(0, 20).forEach((num) => {
+          this.getItemDetails(num);
+        });
       });
   },
+  updated() {
+    if (this.$route.params.id != this.departmentId) {
+      this.departmentId = this.$route.params.id;
+      const url = `https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=${this.$route.params.id}`;
+      fetch(url)
+        .then((response) => response.json())
+        .then((response) => {
+          this.objectIds = response.objectIDs.slice(0, 20);
+          this.objects = [];
+          response.objectIDs.slice(0, 20).forEach((num) => {
+            this.getItemDetails(num);
+          });
+        });
+    };
+  },
+       
 };
 </script>
